4f5e1def3e2a96dfdeca227b63d48520aa95c1c5
officialrishabhgupta <rishabhgupta7210012474@gmail.com>
2021-12-30 22:16:08 +0530

    added

--- src/Container/App.js
+++ src/Container/App.js
@@ -1,16 +1,35 @@
 import React,{Component} from 'react';
-import Navigation from '../Components/Navigation';
-import Logo from '../Components/Logo';
+import Navigation from '../Components/Navigation/Navigation';
+import Logo from '../Components/Logo/Logo';
+import Rank from '../Components/Rank/Rank';
+import ImageLinkForm from '../Components/ImageLinkForm/ImageLinkForm';
+import Particles from 'react-particles-js';
 import './App.css';
 
+const particlesOptions ={
+  particles: {
+    number: {
+      value: 50,
+      density:{
+        enable:true,
+        value_area:800
+      }
+    }
+  }
+}
 class App extends Component {
-  render(){
+  render(){    
   return (
     <div className="App">
+
+      <Particles className='particles'
+      params={particlesOptions} 
+      />
       <Navigation/>
       <Logo/>
-      {/* <ImageLinkForm/>
-      <FaceRecognition/> */}
+      <Rank/>
+      <ImageLinkForm/>
+      {/* <FaceRecognition/> */}
     </div>
   );
 }
